0a70c7750703370d3432ee69e672a580d1116a58
Chan Eng Chien <chanengchien@Chans-MacBook-Pro.local>
2019-04-13 11:03:44 +0800

    I drew a triangle

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 console.log("test...");
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
+var ctx2 = c.getContext("2d")
+
+// ctx.beginPath();
+// ctx.arc(150,150,120,0,6.28);
+// ctx.closePath();
+// ctx.stroke();
+// ctx.fillStyle = "deeppink";
+// ctx.fill();
 
 ctx.beginPath();
-ctx.arc(150,150,120,0,6.28);
+ctx.stokeStyle = "deeppink";
+ctx.moveTo(150,25);
+ctx.lineTo(225,150);
+ctx.lineTo(75,150);
+// ctx1.lineTo(110,50);
 ctx.closePath();
-ctx.stroke();
-ctx.fillStyle = "deeppink";
+ctx.stroke(); 
+ctx.fillStyle = "pink";
 ctx.fill();
